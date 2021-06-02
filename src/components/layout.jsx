f3c7375df45b0e8c85cde223136b5f52ed36ca69
Elisa <elisa.naskali@aalto.fi>
2021-06-02 16:02:47 +0300

    Add very basic global styling

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
+import '../styles/variables.css';
+import '../styles/globals.css';
+
 export default function Layout({ children }) {
   return (
-    <div style={{
-      margin: '0 auto', maxWidth: 650, padding: '0 1rem', backgroundColor: 'lightgray',
-    }}
-    >
+    <div className="layout">
       <nav>
         <Link to="/">Index</Link>
         {' '}
         <Link to="/docs/test-article">Markdown Test</Link>
+        {' '}
+        <Link to="/formPage">Form Demo</Link>
       </nav>
       {children}
     </div>

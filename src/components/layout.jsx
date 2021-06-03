6659f6e05ef955fb0f18f01dd4aede55122d2733
elisa-n <33487024+elisa-n@users.noreply.github.com>
2021-06-03 07:59:44 +0300

    Global styles (#2)
    * Add very basic global styling
    
    * Fix formatting and remove broken link

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
+import '../styles/variables.css';
+import '../styles/globals.css';
+
 export default function Layout({ children }) {
   return (
-    <div style={{
-      margin: '0 auto', maxWidth: 650, padding: '0 1rem', backgroundColor: 'lightgray',
-    }}
-    >
+    <div className="layout">
       <nav>
         <Link to="/">Index</Link>
         {' '}

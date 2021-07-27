88a7572b7549f76cbd797e718c80d578d75e458f
elisa-n <33487024+elisa-n@users.noreply.github.com>
2021-07-27 16:57:38 +0300

    Typography and buttons (#5)
    * Add base font and text styles
    
    * Add and style button components
    
    * Add font import to jest config mocks

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
+import '@fontsource/rubik';
+
 import 'normalize.css';
 import '../styles/globals/global-styles.scss';
 import { layout } from '../styles/layout.module.scss';
@@ -9,10 +11,12 @@ import { layout } from '../styles/layout.module.scss';
 export default function Layout({ children }) {
   return (
     <div className={layout}>
-      <nav>
-        <Link to="/">Index</Link>{' '}
-        <Link to="/docs/test-article">Markdown Test</Link>
-      </nav>
+      <header>
+        <nav>
+          <Link to="/">Index</Link>
+          <Link to="/docs/test-article">Markdown Test</Link>
+        </nav>
+      </header>
       {children}
     </div>
   );

8addc14342f9f4677e0a0225eee64b3d2c77a16e
Tuomas Salminen <tuomas.t.salminen@aalto.fi>
2021-07-27 18:06:53 +0300

    Added arrow link

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,17 +3,20 @@ import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
 import '@fontsource/rubik';
+import LinkWithArrow from './LinkWithArrow';
 
 import 'normalize.css';
 import '../styles/globals/global-styles.scss';
-import { layout } from '../styles/layout.module.scss';
+import { layout, nav } from '../styles/layout.module.scss';
 
 export default function Layout({ children }) {
   return (
     <div className={layout}>
       <header>
-        <nav>
+        <nav className={nav}>
           <Link to="/">Index</Link>
+          <LinkWithArrow to="/docs/test-article">Markdown test</LinkWithArrow>
+          <LinkWithArrow to="/docs/test-article" type='secondary'>Markdown test</LinkWithArrow>
           <Link to="/docs/test-article">Markdown Test</Link>
         </nav>
       </header>

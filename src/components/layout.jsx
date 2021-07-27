bd759ccdd89599f9c1d8dfb728fcdec7a2b112dd
Elisa <elisa.naskali@aalto.fi>
2021-07-27 09:56:26 +0300

    Add base font and text styles

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
+import '@fontsource/rubik';
+
 import 'normalize.css';
 import '../styles/globals/global-styles.scss';
 import { layout } from '../styles/layout.module.scss';
@@ -10,7 +12,7 @@ export default function Layout({ children }) {
   return (
     <div className={layout}>
       <nav>
-        <Link to="/">Index</Link>{' '}
+        <Link to="/">Index</Link>
         <Link to="/docs/test-article">Markdown Test</Link>
       </nav>
       {children}

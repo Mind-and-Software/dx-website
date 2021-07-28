b9f9b3f8ceeffd0019b3393d5b9cfb3b8484985a
Elisa <elisa.naskali@aalto.fi>
2021-07-28 11:48:40 +0300

    Dont render empty blurbs

--- src/components/Blurb.jsx
+++ src/components/Blurb.jsx
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 
 import { blurb } from '../styles/blurb.module.scss';
 
-const Blurb = ({ children }) => <div className={blurb}>{children}</div>;
+const Blurb = ({ children }) => {
+  const isEmpty = children === '';
+
+  return isEmpty ? '' : <div className={blurb}>{children}</div>;
+};
 
 Blurb.defaultProps = {
   children: '',

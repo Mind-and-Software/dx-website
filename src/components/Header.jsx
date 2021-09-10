f511f6bf2c1075261c49752877d13fbda394d2ce
Tuomas Salminen <tuomas.t.salminen@aalto.fi>
2021-09-10 15:39:03 +0300

    Changes to test CI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
-import Navbar from '../components/Navbar';
+import Navbar from './Navbar';
 
 const Header = () => {
   const data = useStaticQuery(graphql`
